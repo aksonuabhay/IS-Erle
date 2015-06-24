68781aaaa37ecd3c564b0cf0d82c9e0ae4ee8241
Abhay Kumar <aksonuabhay@gmail.com>
2015-06-24 22:59:06 +0530

    Add Udp Clent features

--- is.erle.comms/src/main/java/is/erle/comms/IsErleCommsActivity.java
+++ is.erle.comms/src/main/java/is/erle/comms/IsErleCommsActivity.java
@@ -1,18 +1,52 @@
 package is.erle.comms;
 
+import java.net.InetSocketAddress;
+
 import interactivespaces.activity.impl.BaseActivity;
+import interactivespaces.service.comm.network.client.UdpClientNetworkCommunicationEndpoint;
+import interactivespaces.service.comm.network.client.UdpClientNetworkCommunicationEndpointListener;
+import interactivespaces.service.comm.network.client.UdpClientNetworkCommunicationEndpointService;
 
 /**
  * A simple Interactive Spaces Java-based activity.
  */
 public class IsErleCommsActivity extends BaseActivity {
 
+	private static final String CONFIGURATION_SERVER_HOST = "space.comm.udp.server.host";
+	private static final String CONFIGURATION_SERVER_PORT = "space.comm.udp.server.port";
+	
+	
+	
+	private UdpClientNetworkCommunicationEndpoint udpDroneClient;
+	private InetSocketAddress udpDroneServerAddress;
+	
+	
     @Override
     public void onActivitySetup() {
         getLog().info("Activity is.erle.comms setup");
+        UdpClientNetworkCommunicationEndpointService udpDroneClientService = 
+        		getSpaceEnvironment().getServiceRegistry().getRequiredService(UdpClientNetworkCommunicationEndpointService.SERVICE_NAME);
+        
+        String udpDroneServerHost = getConfiguration().getRequiredPropertyString(CONFIGURATION_SERVER_HOST);
+        int udpDroneServerPort = getConfiguration().getRequiredPropertyInteger(CONFIGURATION_SERVER_PORT);
+        
+        udpDroneClient = udpDroneClientService.newClient(getLog());
+        udpDroneClient.addListener(new UdpClientNetworkCommunicationEndpointListener() {
+			
+			public void onUdpResponse(UdpClientNetworkCommunicationEndpoint arg0,
+					byte[] response, InetSocketAddress address) {
+				// TODO Auto-generated method stub
+				handleUdpDroneResponse(response , address);
+			}
+		});
+        addManagedResource(udpDroneClient);
+        
+        udpDroneServerAddress = new InetSocketAddress(udpDroneServerHost, udpDroneServerPort);
     }
 
-    @Override
+
+
+	@Override
     public void onActivityStartup() {
         getLog().info("Activity is.erle.comms startup");
     }
@@ -46,4 +80,10 @@ public class IsErleCommsActivity extends BaseActivity {
     public void onActivityCleanup() {
         getLog().info("Activity is.erle.comms cleanup");
     }
+    
+    protected void handleUdpDroneResponse(byte[] response,
+			InetSocketAddress address) {
+		// TODO Auto-generated method stub
+		
+	}
 }
