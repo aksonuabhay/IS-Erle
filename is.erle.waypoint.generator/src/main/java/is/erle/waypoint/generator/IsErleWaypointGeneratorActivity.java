0e23c8fd61d71e9388f227bf796a66cbac2dab99
Abhay Kumar <aksonuabhay@gmail.com>
2015-07-30 18:29:41 +0530

    Improve file checking by checking column coordinate frame

--- is.erle.waypoint.generator/src/main/java/is/erle/waypoint/generator/IsErleWaypointGeneratorActivity.java
+++ is.erle.waypoint.generator/src/main/java/is/erle/waypoint/generator/IsErleWaypointGeneratorActivity.java
@@ -52,7 +52,7 @@ public class IsErleWaypointGeneratorActivity extends BaseRoutableRosActivity
         publisherName = getConfiguration().getRequiredPropertyString(CONFIGURATION_PUBLISHER_NAME);
         subscriberName = getConfiguration().getRequiredPropertyString(CONFIGURATION_SUBSCRIBER_NAME);
         fileWithDirectory = getActivityFilesystem().getInstallDirectory().getAbsolutePath()+"/"+FILE_NAME;
-        short lineCount=1;
+        int lineCount=1;
         try 
         {
 			br = new BufferedReader(new FileReader(fileWithDirectory));
@@ -63,13 +63,19 @@ public class IsErleWaypointGeneratorActivity extends BaseRoutableRosActivity
 					currentLine =currentLine.trim();
 					if (!checkColumnLength(currentLine))
 					{
-						getLog().error("Aborting file read due to clomun length inconsistency");
+						getLog().error("Aborting file read due to column length inconsistency");
 						return;
 					}
 					
 					if (!checkColumnContent(currentLine))
 					{
-						getLog().error("Aborting file read due to clomun content inconsistency");
+						getLog().error("Aborting file read due to column content inconsistency");
+						return;
+					}
+					
+					if (!checkCoordinateFrame(currentLine))
+					{
+						getLog().error("Aborting file read due to coordinate frame inconsistency");
 						return;
 					}
 					
@@ -78,9 +84,9 @@ public class IsErleWaypointGeneratorActivity extends BaseRoutableRosActivity
 
 				lineCount++;
 			}
-			if (lineCount != (waypointCount+2)) 
+			if (lineCount != (waypointCount+3)) 
 			{
-				getLog().info("Waypoint count and number of lines mismatch , recheck mission file ");
+				getLog().warn("Waypoint count and number of lines mismatch , recheck mission file ");
 			}
 			br.close();
 		} 
@@ -272,6 +278,186 @@ public class IsErleWaypointGeneratorActivity extends BaseRoutableRosActivity
 		{
 			return false;
 		}
+	}
+	
+	private boolean isInteger(String value)
+	{
+		try
+		{
+			Integer.parseInt(value);
+			return true;
+		}
+		catch (NumberFormatException e)
+		{
+			return false;
+		}
+	}
+	
+	private boolean checkCoordinateFrame(String column)
+	{
+		/*
+		 * MAV_FRAME_GLOBAL = 0; Global coordinate frame, WGS84 coordinate
+		 * system. First value / x: latitude, second value / y: longitude, third
+		 * value / z: positive altitude over mean sea level (MSL)
+		 * 
+		 * MAV_FRAME_LOCAL_NED = 1; Local coordinate frame, Z-up (x: north, y:
+		 * east, z: down).
+		 * 
+		 * MAV_FRAME_MISSION = 2; NOT a coordinate frame, indicates a mission
+		 * command.
+		 * 
+		 * MAV_FRAME_GLOBAL_RELATIVE_ALT = 3; Global coordinate frame, WGS84
+		 * coordinate system, relative altitude over ground with respect to the
+		 * home position. First value / x: latitude, second value / y:
+		 * longitude, third value / z: positive altitude with 0 being at the
+		 * altitude of the home location.
+		 * 
+		 * MAV_FRAME_LOCAL_ENU = 4; Local coordinate frame, Z-down (x: east, y:
+		 * north, z: up)
+		 * 
+		 * MAV_FRAME_GLOBAL_INT = 5; Global coordinate frame, WGS84 coordinate
+		 * system. First value / x: latitude in degrees*1.0e-7, second value /
+		 * y: longitude in degrees*1.0e-7, third value / z: positive altitude
+		 * over mean sea level (MSL)
+		 * 
+		 * MAV_FRAME_GLOBAL_RELATIVE_ALT_INT = 6; Global coordinate frame, WGS84
+		 * coordinate system, relative altitude over ground with respect to the
+		 * home position. First value / x: latitude in degrees*10e-7, second
+		 * value / y: longitude in degrees*10e-7, third value / z: positive
+		 * altitude with 0 being at the altitude of the home location.
+		 * 
+		 * MAV_FRAME_LOCAL_OFFSET_NED = 7; Offset to the current local frame.
+		 * Anything expressed in this frame should be added to the current local
+		 * frame position.
+		 * 
+		 * MAV_FRAME_BODY_NED = 8; Setpoint in body NED frame. This makes sense
+		 * if all position control is externalized - e.g. useful to command 2
+		 * m/s^2 acceleration to the right.
+		 * 
+		 * MAV_FRAME_BODY_OFFSET_NED = 9; Offset in body NED frame. This makes
+		 * sense if adding setpoints to the current flight path, to avoid an
+		 * obstacle - e.g. useful to command 2 m/s^2 acceleration to the east.
+		 * 
+		 * MAV_FRAME_GLOBAL_TERRAIN_ALT = 10; Global coordinate frame with above
+		 * terrain level altitude. WGS84 coordinate system, relative altitude
+		 * over terrain with respect to the waypoint coordinate. First value /
+		 * x: latitude in degrees, second value / y: longitude in degrees, third
+		 * value / z: positive altitude in meters with 0 being at ground level
+		 * in terrain model.
+		 * 
+		 * MAV_FRAME_GLOBAL_TERRAIN_ALT_INT = 11; Global coordinate frame with
+		 * above terrain level altitude. WGS84 coordinate system, relative
+		 * altitude over terrain with respect to the waypoint coordinate. First
+		 * value / x: latitude in degrees*10e-7, second value / y: longitude in
+		 * degrees*10e-7, third value / z: positive altitude in meters with 0
+		 * being at ground level in terrain model.
+		 * 
+		 * MAV_FRAME_ENUM_END = 12;
+		 */
+		String[] splitColumn = column.trim().split(SEPARATOR);
+		int coordinateFrame;
+
+		if (isInteger(splitColumn[2]))
+		{
+			coordinateFrame = Integer.parseInt(splitColumn[2]);
+		}
+		else
+		{
+			getLog().error("Coordinate frame not parseable to int.");
+			return false;
+		}
+		if (coordinateFrame < 0 | coordinateFrame >= 12 | coordinateFrame == 2)
+		{
+			getLog().error(
+					splitColumn[0]
+							+ " column does not contain a valid coordinate frame.");
+			return false;
+		}
+
+		if (splitColumn[0].equals("0"))
+		{
+			if ((coordinateFrame != 0) & (coordinateFrame != 5))
+			{
+				getLog().error(
+						"First column does not contain home coordinates in Global Coordinate system.");
+				return false;
+			}
+			else
+			{
+				if (coordinateFrame == 5)
+				{
+					if (!isLatLonAltInt(splitColumn))
+					{
+						return false;
+					}
+				}
+			}
+		}
+		else
+		{
+			if ((coordinateFrame != 3) & (coordinateFrame != 6))
+			{
+				getLog().warn(
+						splitColumn[0]
+								+ " column does not contain coordinates in Global Relative Altitude Coordinate system.");
+			}
+			else
+			{
+				if (coordinateFrame == 6 | coordinateFrame == 11 | coordinateFrame == 5)
+				{
+					if (!isLatLonAltInt(splitColumn))
+					{
+						getLog().error(
+								splitColumn[0]
+										+ " column has double values but coordinate frame specified as integer.");
+						return false;
+					}
+				}
+				if (coordinateFrame == 8)
+				{
+					getLog().error(
+							splitColumn[0]
+									+ " column uses BODY_NED coordinate frame. This is to be used in cases involving external position control using accelaration control and not a mission.");
+					return false;
+				}
+				if (coordinateFrame == 9)
+				{
+					getLog().error(
+							splitColumn[0]
+									+ " column uses BODY_OFFSET_NED coordinate frame. This is to be used in cases involving obstacle avoidance using accelearation control and not a mission.");
+					return false;
+				}
+				if (coordinateFrame == 7)
+				{
+					getLog().error(
+							splitColumn[0]
+									+ " column uses LOCAL_OFFSET_NED coordinate frame. This is to be used in a mission.");
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private boolean isLatLonAltInt(String [] columnArray)
+	{
+		boolean flag = true;
+		if (!isInteger(columnArray[8]))
+		{
+			getLog().error("Longitude in double but coordinate frame in Global Integer.");
+			flag = false;
+		}
+		if (!isInteger(columnArray[9]))
+		{
+			getLog().error("Latitude in double but coordinate frame in Global Integer.");
+			flag = false;
+		}
+		if (!isInteger(columnArray[10]))
+		{
+			getLog().error("Altitude in double but coordinate frame in Global Integer.");
+			flag = false;
+		}
+		return flag;
 	}
 }
 
