ebfb27995bdd66998c6c45606b7c9332ff34349e
Abhay Kumar <aksonuabhay@gmail.com>
2015-08-18 13:28:42 +0530

    Update to shut down and startup everything with proper messages

--- start.py
+++ start.py
@@ -1,11 +1,19 @@
 import json
 import requests
 import time
-
+import signal
+import sys
+import subprocess
 
 session=requests.Session()
 url_prefix = "http://127.0.0.1:8080/interactivespaces/"
 activity_id = {}#{'captain':'0','serial':'0','udp':'0','mavlink':'0','generator':'0','processor':'0'}
+comms_port ='serial'
+
+def signal_handler(signal, frame):
+    print "Closing everything"
+    exit_sequence(comms_port)
+    sys.exit(0)
 
 def make_request(url):
     live_activity=requests.get(url).content
@@ -31,7 +39,7 @@ def url_status(uid):
 
 def process_result(result_data):
     if result_data['result']=='success':
-        print "Success"
+        #print "Success"
         return 'success'
     else:
         print "Could not contact master properly"
@@ -39,30 +47,72 @@ def process_result(result_data):
 
 def launch_sequence(comms_port):
     if process_result(make_request(url_prefix+url_startup(activity_id['mavlink']))) == 'success':
-        time.sleep(3)
+        print "Success in starting mavlink activity"
+        time.sleep(4)
         if process_result(make_request(url_prefix+url_startup(activity_id[comms_port]))) == 'success':
+            print "Success in starting communications activity"
             time.sleep(1)
             if process_result(make_request(url_prefix+url_startup(activity_id['generator']))) == 'success':
+                print "Success in starting waypoint generator activity"
                 time.sleep(1)
                 if process_result(make_request(url_prefix+url_activate(activity_id['mavlink']))) == 'success':
-                    time.sleep(0.5)
+                    print "Success in activating mavlink activity"
+                    time.sleep(1)
                     if process_result(make_request(url_prefix+url_activate(activity_id[comms_port]))) == 'success':
-                        time.sleep(0.5)
+                        print "Success in activating communications activity"
+                        time.sleep(1)
                         if process_result(make_request(url_prefix+url_activate(activity_id['generator']))) == 'success':
+                            print "Success in activating waypoint generator activity"
                             time.sleep(2)
                             if process_result(make_request(url_prefix+url_startup(activity_id['captain']))) == 'success':
+                                print "Success in starting captain activity"
                                 time.sleep(2)
                                 if process_result(make_request(url_prefix+url_activate(activity_id['captain']))) == 'success':
+                                    print "Success in activating captain activity"
                                     time.sleep(0.5)
                                     if process_result(make_request(url_prefix+url_startup(activity_id['processor']))) == 'success':
+                                        print "Success in starting waypoint processor activity"
                                         time.sleep(1)
                                         if process_result(make_request(url_prefix+url_activate(activity_id['processor']))) == 'success':
+                                            print "Success in activating waypoint processor activity"
                                             time.sleep(0.5)
                                             print 'Launched all the activities successfully'
+                                            return
+    print "Could not start all the activities"
+
+def exit_sequence(comms_port):
+    if process_result(make_request(url_prefix+url_shutdown(activity_id['processor']))) == 'success':
+        print "Shutting down waypoint processor activity"
+        time.sleep(0.5)
+        if process_result(make_request(url_prefix+url_shutdown(activity_id['captain']))) == 'success':
+            print "Shutting down captain activity"
+            time.sleep(10)
+            if process_result(make_request(url_prefix+url_shutdown(activity_id['generator']))) == 'success':
+                print "Shutting down waypoint generator activity"
+                time.sleep(0.5)
+                if process_result(make_request(url_prefix+url_shutdown(activity_id[comms_port]))) == 'success':
+                    print "Shutting down communications activity"
+                    time.sleep(1)
+                    if process_result(make_request(url_prefix+url_shutdown(activity_id['mavlink']))) == 'success':
+                        print "Shutting down mavlink activity"
+                        time.sleep(2)
+                        subprocess._cleanup()
+                        print "Everything shut down"
+                        return
+    print "Could not shutdown all the activities"
+
 #url_live_activity = "http://127.0.0.1:8080/interactivespaces/liveactivity/all.json";
 #live_activity=requests.get(url_prefix+"liveactivity/all.json").content
 #print live_activity
-data= make_request(url_prefix+"liveactivity/all.json")
+data = {}
+try:
+    data= make_request(url_prefix+"liveactivity/all.json")
+except requests.exceptions.ConnectionError:
+    subprocess.call(['gnome-terminal', '-x', './../../master/bin/isstartup'])
+    time.sleep(10)
+    subprocess.call(['gnome-terminal', '-x', './../../controller/bin/isstartup'])
+    time.sleep(10)
+    data= make_request(url_prefix+"liveactivity/all.json")
 
 
 if data['result']=='success':
@@ -83,7 +133,10 @@ if data['result']=='success':
     #print activity_id['captain']
     if len(activity_id)>=5:
         print 'Starting all the activities'
-        launch_sequence('serial')
+        launch_sequence(comms_port)
+        signal.signal(signal.SIGINT, signal_handler)
+        print('Press Ctrl+C to exit')
+        signal.pause()
     else:
         print 'Not all the activities are installed'
         print 'Only the following activities are installed, Install rest of them'
